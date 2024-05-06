5e89f39ad729ab0e4e9fdda36c691f105c907bce
Andrei <93013795+oliam9@users.noreply.github.com>
2024-05-06 16:58:21 +0000

    ready1

--- app.py
+++ app.py
@@ -1,10 +1,43 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+JOBS = [
+    {
+        'id': 1,
+        'title': 'Data Analyst',
+        'location': 'Dubai, UAE',
+        'salary': 'AED12000'
+   },
+    {
+            'id': 2,
+            'title': 'Tester',
+            'location': 'Remote',
+            'salary': 'E1500'
+       },
+    {
+            'id': 3,
+            'title': 'Front-End',
+            'location': 'Dublin, Ireland',
+            'salary': '&1200'
+       },
+    {
+            'id': 4,
+            'title': 'Full-Stack',
+            'location': 'SF, California',
+            'salary': '$4000'
+       },
+    {
+            'id': 5,
+            'title': 'Back-end',
+            'location': 'London, UK',
+            'salary': '&2500'
+       },
+]
+
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return render_template('home.html', jobs=JOBS)
 
 
 if __name__ == "__main__":
