7a0de93eb1b1b6a7fca042b5abdd2baf70cbe87b
Andrei <93013795+oliam9@users.noreply.github.com>
2024-05-07 12:14:39 +0000

    added connection to db

--- app.py
+++ app.py
@@ -1,43 +1,12 @@
 from flask import Flask, render_template
+from database import load_jobs_from_db
 
 app = Flask(__name__)
 
-JOBS = [
-    {
-        'id': 1,
-        'title': 'Data Analyst',
-        'location': 'Dubai, UAE',
-        'salary': 'AED12000'
-   },
-    {
-            'id': 2,
-            'title': 'Tester',
-            'location': 'Remote',
-            'salary': 'E1500'
-       },
-    {
-            'id': 3,
-            'title': 'Front-End',
-            'location': 'Dublin, Ireland',
-            'salary': '&1200'
-       },
-    {
-            'id': 4,
-            'title': 'Full-Stack',
-            'location': 'SF, California',
-            'salary': '$4000'
-       },
-    {
-            'id': 5,
-            'title': 'Back-end',
-            'location': 'London, UK',
-            'salary': '&2500'
-       },
-]
-
 @app.route("/")
 def hello_world():
-    return render_template('home.html', jobs=JOBS)
+    jobs = load_jobs_from_db()
+    return render_template('home.html', jobs=jobs)
 
 
 if __name__ == "__main__":
