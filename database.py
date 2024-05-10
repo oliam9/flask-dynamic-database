953f7eed239002d2db2b407821b8446ef13c3cd5
Andrei <93013795+oliam9@users.noreply.github.com>
2024-05-10 21:20:45 +0000

    first test end

--- database.py
+++ database.py
@@ -21,3 +21,14 @@ def load_job_from_db(id):
             return None
         else:
             return dict(rows[0])
+
+def add_application_to_db(job_id, data):
+  with engine.connect() as conn:
+    query = text("INSERT INTO applications (job_id, full_name, email, education, work_experience) VALUES (:job_id, :full_name, :email, :education, :work_experience)")
+      
+    conn.execute(query, 
+                 job_id=job_id,
+                 full_name=data['full_name'],
+                 email=data['email'],
+                 education=data['education'],
+                 work_experience=data['work_experience'])
