814504bc77c095f9981270934d736c0d59f4b18b
Andrei <93013795+oliam9@users.noreply.github.com>
2024-05-10 20:05:54 +0000

    latest - job page

--- app.py
+++ app.py
@@ -12,7 +12,9 @@ def hello_world():
 @app.route("/job/<id>")
 def show_job(id):
     job = load_job_from_db(id)
-    return jsonify(job)
+    if not job:
+        return "Not Found", 404
+    return render_template('jobpage.html', job=job)
 
 
 if __name__ == "__main__":
--- database.py
+++ database.py
@@ -16,7 +16,7 @@ def load_jobs_from_db():
 def load_job_from_db(id):
     with engine.connect() as conn:
         result = conn.execute(text("select * from jobs where id = :val"), {"val": id})
-        rows = result.all()
+        rows = result.mappings().all()
         if len(rows) == 0:
             return None
         else:
